e71fbb4c42db270fc4abe031e8d6401e378d1104
Dominique Marcadet <Dominique.Marcadet@centralesupelec.fr>
2019-05-31 12:37:56 +0200

    version is a6 31/05/2019

--- fr.centralesupelec.edf.riseclipse.iec61850.scl.tools/src/fr/centralesupelec/edf/riseclipse/iec61850/scl/validator/RiseClipseValidatorSCL.java
+++ fr.centralesupelec.edf.riseclipse.iec61850.scl.tools/src/fr/centralesupelec/edf/riseclipse/iec61850/scl/validator/RiseClipseValidatorSCL.java
@@ -303,7 +303,7 @@ public class RiseClipseValidatorSCL {
         console.info( "Web site:" );
         console.info( "    http://wdi.supelec.fr/software/RiseClipse/" );
         console.info( "" );
-        console.info( "RiseClipseValidatorSCL version: 1.1.0 a5 (23 may 2019)" );
+        console.info( "RiseClipseValidatorSCL version: 1.1.0 a6 (31 may 2019)" );
         console.info( "" );
     }
 
