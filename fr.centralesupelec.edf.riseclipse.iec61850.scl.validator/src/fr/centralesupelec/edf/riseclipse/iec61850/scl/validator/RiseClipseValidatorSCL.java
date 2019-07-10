615fb3c50ad817410a9f2f9714f08fcaa0194e6a
Dominique Marcadet <Dominique.Marcadet@centralesupelec.fr>
2019-07-10 09:04:04 +0200

    set license to EPL 2.0

--- fr.centralesupelec.edf.riseclipse.iec61850.scl.validator/src/fr/centralesupelec/edf/riseclipse/iec61850/scl/validator/RiseClipseValidatorSCL.java
+++ fr.centralesupelec.edf.riseclipse.iec61850.scl.validator/src/fr/centralesupelec/edf/riseclipse/iec61850/scl/validator/RiseClipseValidatorSCL.java
@@ -1,21 +1,24 @@
-/**
- *  Copyright (c) 2019 CentraleSupélec & EDF.
- *  All rights reserved. This program and the accompanying materials
- *  are made available under the terms of the Eclipse Public License v1.0
- *  which accompanies this distribution, and is available at
- *  http://www.eclipse.org/legal/epl-v10.html
- * 
- *  This file is part of the RiseClipse tool
- *  
- *  Contributors:
- *      Computer Science Department, CentraleSupélec
- *      EDF R&D
- *  Contacts:
- *      dominique.marcadet@centralesupelec.fr
- *      aurelie.dehouck-neveu@edf.fr
- *  Web site:
- *      http://wdi.supelec.fr/software/RiseClipse/
- */
+/*
+*************************************************************************
+**  Copyright (c) 2019 CentraleSupélec & EDF.
+**  All rights reserved. This program and the accompanying materials
+**  are made available under the terms of the Eclipse Public License v2.0
+**  which accompanies this distribution, and is available at
+**  https://www.eclipse.org/legal/epl-v20.html
+** 
+**  This file is part of the RiseClipse tool
+**  
+**  Contributors:
+**      Computer Science Department, CentraleSupélec
+**      EDF R&D
+**  Contacts:
+**      dominique.marcadet@centralesupelec.fr
+**      aurelie.dehouck-neveu@edf.fr
+**  Web site:
+**      http://wdi.supelec.fr/software/RiseClipse/
+*************************************************************************
+*/
+
 package fr.centralesupelec.edf.riseclipse.iec61850.scl.validator;
 
 import java.util.ArrayList;
@@ -294,10 +297,10 @@ public class RiseClipseValidatorSCL {
         IRiseClipseConsole console = AbstractRiseClipseConsole.getConsole();
         
         console.info( "Copyright (c) 2019 CentraleSupélec & EDF." );
-        console.info(
-                "All rights reserved. This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0" );
-        console.info(
-                "which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html" );
+        console.info( "All rights reserved. This program and the accompanying materials" );
+        console.info( "are made available under the terms of the Eclipse Public License v2.0" );
+        console.info( "which accompanies this distribution, and is available at" );
+        console.info( "https://www.eclipse.org/legal/epl-v20.html" );
         console.info( "" );
         console.info( "This tool is part of RiseClipse." );
         console.info( "Contributors:" );
